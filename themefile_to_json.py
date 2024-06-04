fcf72fe2fdc0cf296bef70977aebcd9bb1a96b96
Triach Rold <156170660+triach-rold@users.noreply.github.com>
2024-06-05 00:17:40 +0530

    Update themefile_to_json.py

--- themefile_to_json.py
+++ themefile_to_json.py
@@ -1,5 +1,6 @@
 import json
 import re
+import sys
 def parse_theme_file(file_content):
     theme_dict = {}
     themes = re.findall(r'(\w[\w-]*):\s*\{([^}]*)\}', file_content, re.DOTALL)
@@ -10,15 +11,16 @@ def parse_theme_file(file_content):
                 key, value = attr.split(':')
                 theme_dict[theme][key.strip()] = value.strip()
     return theme_dict
-
-def convert_to_json(file_path, output_path):
-    with open(file_path, 'r') as file:
+def convert_to_json(input_path, output_path):
+    with open(input_path, 'r') as file:
         file_content = file.read()
     theme_dict = parse_theme_file(file_content)
     with open(output_path, 'w') as json_file:
         json.dump(theme_dict, json_file, indent=4)
-
-input_file = 'themes.txt'
-output_file = 'themes.json'
-convert_to_json(input_file, output_file)
-
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python3 converter.py input.txt output.json")
+    else:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+        convert_to_json(input_file, output_file)
